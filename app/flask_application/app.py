8d9203c6d2b10d5f88e9de5d23e1fff75829e5bb
TERUZ-VTAT <terunori.kato0911@gmail.com>
2023-11-17 12:20:19 +0900

    create header

--- app/flask_application/app.py
+++ app/flask_application/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def HeadPage():
-    return render_template("index.html")
+    return render_template("header.html")
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=80)
