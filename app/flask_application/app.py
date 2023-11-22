211070c05013db2400e55195a186515a48803910
kiichi7580 <kiichi.nakazato0521@gmail.com>
2023-11-22 15:03:53 +0900

    プロフィール画面作成

--- app/flask_application/app.py
+++ app/flask_application/app.py
@@ -5,6 +5,11 @@ app = Flask(__name__)
 @app.route('/')
 def HeadPage():
     return render_template("index.html")
+@app.route('/profile')
+def ProfilePage():
+    return render_template("profile.html")
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=80)
+
+    
